9d070d41b005e99d190c31fec15a2e54228cd5a2
Devar2127 <f20260194@pilani.bits-pilani.ac.in>
2026-09-13 20:26:15 +0530

    Complete 1.2

--- Maincode.py
+++ Maincode.py
@@ -15,7 +15,22 @@ class linear:
         output = X @ self.W + self.b
         return output
 
-class ReLU:
+class ReLU:                                    #Defining Rectified linear unit
     def forward(self, Z):
         self.Z = Z
         return numpy.maximum(0, Z)
+
+#1.2
+
+class Network:
+    def __init__(self, in_features, hidden_features, out_features):
+        self.layer1 = linear(in_features, hidden_features)
+        self.ReLU = ReLU()
+        self.layer2 = linear(hidden_features, out_features)
+
+
+    def forward(self, X):
+        z1 = self.layer1.forward(X)
+        a1 = self.ReLU.forward(z1)
+        z2 = self.layer2.forward(a1)
+        return z2
